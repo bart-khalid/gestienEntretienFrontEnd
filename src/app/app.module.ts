6d0c0af9e46a89a8d16c2a7be97e6f78038f9b1b
zakaria <zikochakir@gmail.com>
2020-04-20 17:02:46 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+    LoginComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+ }
